83f856d2aaa7cffe19cedb884dd0d3dcac8e7a48
zubairmh <zubair.mh@protonmail.com>
2023-06-25 18:49:47 +0530

    start connecting actions to routes

--- internal/actions/document.go
+++ internal/actions/document.go
@@ -2,12 +2,21 @@ package actions
 
 import (
 	"io"
+	"log"
+
+	"github.com/ggbois-inc/docshieldapi/internal/database"
+	"github.com/ggbois-inc/docshieldapi/internal/ipfs"
 )
 
 func UploadDocument(meta_id string, file io.Reader, filename string) {
-
+	log.Printf("Running Upload action for %s", meta_id)
+	database.CreateUser(meta_id)
+	cid := ipfs.UploadFile(file)
+	database.CreateDocument(meta_id, filename, cid, generateRandomString(10))
 }
 
-func GetDocuments(meta_id string) {
-
+func GetDocuments(meta_id string) []database.Document {
+	log.Printf("Running Get Files action for %s", meta_id)
+	docs := database.GetDocuments(meta_id)
+	return docs
 }
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,16 +1,42 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/ggbois-inc/docshieldapi/internal/actions"
+	driver "github.com/ggbois-inc/docshieldapi/internal/database"
 	"github.com/julienschmidt/httprouter"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Hello world\n")
+	u := driver.CreateUser("xyz")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(u)
+}
+
+func fileUpload(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	actions.UploadDocument(r.Header.Get("meta_id"), file, fileHeader.Filename)
+	fmt.Fprint(w, "DONE\n")
+}
+
+func getFiles(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	docs := actions.GetDocuments(r.Header.Get("meta_id"))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(docs)
 }
 
 func createRoutes(router *httprouter.Router) {
 	router.GET("/", homePage)
+	router.POST("/api/upload", fileUpload)
+	router.GET("/api/files", getFiles)
 }
