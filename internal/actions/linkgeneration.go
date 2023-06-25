bc9fcc654b281c82f63ef39c9f8a3aa36b782d9e
zubairmh <zubair.mh@protonmail.com>
2023-06-25 18:12:47 +0530

    get document by shortlink

--- internal/actions/linkgeneration.go
+++ internal/actions/linkgeneration.go
@@ -4,6 +4,9 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"log"
+
+	"github.com/ggbois-inc/docshieldapi/internal/database"
+	"github.com/ggbois-inc/docshieldapi/internal/ipfs"
 )
 
 func generateRandomString(length int) string {
@@ -15,6 +18,8 @@ func generateRandomString(length int) string {
 	return base64.URLEncoding.EncodeToString(bytes)[:length]
 }
 
-func GetDocumentByLink(shortcode string) {
-
+func GetDocumentByLink(shortcode string) []byte {
+	doc := database.GetDocumentByCode(shortcode)
+	file := ipfs.GetFile(doc.CID)
+	return file
 }
--- internal/database/mongo_driver.go
+++ internal/database/mongo_driver.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
-
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,6 +16,21 @@ import (
 var users *mongo.Collection
 var documents *mongo.Collection
 var ctx context.Context = context.TODO()
+
+type Permission struct {
+	MetaMaskID string `bson:"meta_id" json:"meta_id"`
+	Perm       int    `bson:"permission" json:"permission"`
+}
+
+type Document struct {
+	ID          primitive.ObjectID `bson:"_id" json:"_id"`
+	Filename    string             `bson:"filename" json:"filename"`
+	CID         string             `bson:"cid" json:"cid"`
+	ShortCode   string             `bson:"short" json:"short"`
+	CreatedBy   string             `bson:"meta_id" json:"meta_id"`
+	CreatedOn   time.Time          `bson:"created_on" json:"created_on"`
+	Permissions []Permission       `bson:"permissions" json:"permissions"`
+}
 
 func init() {
 	godotenv.Load()
@@ -26,4 +43,13 @@ func init() {
 	log.Println("Database Loaded")
 	users = client.Database("blockdb").Collection("users")
 	documents = client.Database("blockdb").Collection("documents")
+}
+
+func GetDocumentByCode(shortcode string) Document {
+	var result Document
+	err := documents.FindOne(ctx, bson.D{primitive.E{Key: "short", Value: shortcode}}).Decode(&result)
+	if err == nil {
+		return result
+	}
+	return Document{}
 }
