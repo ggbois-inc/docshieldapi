327fa16d3f75fe1749981e6b25e858e0132c856c
zubairmh <zubair.mh@protonmail.com>
2023-06-25 17:21:51 +0530

    load mongo credentials from env

--- internal/database/mongo_driver.go
+++ internal/database/mongo_driver.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var users *mongo.Collection
+var documents *mongo.Collection
+var ctx context.Context = context.TODO()
+
+func init() {
+	godotenv.Load()
+	uri := os.Getenv("MONGO")
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Database Loaded")
+	users = client.Database("blockdb").Collection("users")
+	documents = client.Database("blockdb").Collection("documents")
+}
